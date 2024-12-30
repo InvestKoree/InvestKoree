f6988cd52df4a111339ce74b44e7b916f150a7c5
unknown <arnobm97@gmail.com>
2024-12-30 11:07:14 +0600

    asdasad

--- client-side/src/Pages/Blogs.jsx
+++ client-side/src/Pages/Blogs.jsx
@@ -67,7 +67,7 @@ const Blogs = () => {
             {/* Image and Text Container */}
             <div className="flex flex-col md:flex-row h-full p-4">
               {/* Image */}
-              <div className="lg:w-full md:w-1/2 h-64 md:h-auto sm:w-[60%] xxs:w-[60%] xs:w-[60%]  sm:mb-2 xxs:mb-2 xs:mb-2 sm:h-[60%] xxs:h-[60%] xs:h-[60%]">
+              <div className="lg:w-full md:w-1/2 h-64 md:h-auto   sm:mb-2 xxs:mb-2 xs:mb-2 sm:h-[60%] xxs:h-[60%] xs:h-[60%]">
                 <img
                   src={temp}
                   className="object-cover w-full h-full rounded-md"
@@ -112,7 +112,7 @@ const Blogs = () => {
             {/* Image and Text Container */}
             <div className="flex flex-col md:flex-row h-full p-4">
               {/* Image */}
-              <div className="lg:w-full md:w-1/2 h-64 md:h-auto sm:w-[60%] xxs:w-[60%] xs:w-[60%]  sm:mb-2 xxs:mb-2 xs:mb-2 sm:h-[60%] xxs:h-[60%] xs:h-[60%]">
+              <div className="lg:w-full md:w-1/2 h-64 md:h-auto   sm:mb-2 xxs:mb-2 xs:mb-2 sm:h-[60%] xxs:h-[60%] xs:h-[60%]">
                 <img
                   src={temp}
                   className="object-cover w-full h-full rounded-md"
