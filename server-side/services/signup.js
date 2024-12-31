89d740ecc1dc850a2ac4b19948696e60acae29fc
unknown <arnobm97@gmail.com>
2024-12-31 12:19:59 +0600

    asdads

--- server-side/services/signup.js
+++ server-side/services/signup.js
@@ -1,24 +1,21 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcrypt';
 
-async function createUser (userData) {
-    const { name, email, password, role, phone } = userData;
 
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user
-    const newUser  = new User({
+async function  createUser(userData) {
+    const{name,email,password} =userData;
+    const hashedPassword =bcrypt.hash(password,10);
+    const createUser = new User({
         name,
         email,
-        password: hashedPassword,
-        role,
-        phone
+        password :hashedPassword,
+        role ,phone
     });
 
-    // Save the user to the database
-    const savedUser  = await newUser .save();
-    return savedUser ;
+    const savedUser = await createUser.save();
+    return savedUser;
+
 }
 
-export default createUser ;
+export default createUser;
