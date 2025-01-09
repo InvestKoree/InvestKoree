33ea175c5f4bf3c4c179732022601a2e6e37a69e
unknown <arnobm97@gmail.com>
2025-01-09 14:11:38 +0600

    dasd

--- server-side/cloudinaryConfig.js
+++ server-side/cloudinaryConfig.js
@@ -2,7 +2,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Your Cloudinary cloud name
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Your Cloudinary clod name
   api_key: process.env.CLOUDINARY_API_KEY, // Your Cloudinary API key
   api_secret: process.env.CLOUDINARY_API_SECRET, // Your Cloudinary API secret
 });
