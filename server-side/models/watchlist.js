dfa41ebf19f83f090c2f1fd1933d352c2eae4620
unknown <arnobm97@gmail.com>
2025-01-21 13:40:16 +0600

    asdasd

--- server-side/models/watchlist.js
+++ server-side/models/watchlist.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const watchlistSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FounderPending' }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FounderPost' }],
 });
 
 const Watchlist = mongoose.model('Watchlist', watchlistSchema);
