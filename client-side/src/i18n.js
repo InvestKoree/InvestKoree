c89034e0d30831d181cdf102cccbdc50a5c55930
unknown <arnobm97@gmail.com>
2025-02-11 12:31:47 +0600

    asdsa

--- client-side/src/i18n.js
+++ client-side/src/i18n.js
@@ -2,16 +2,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-
+import en from "./locales/en/translation.json";
+import bn from "./locales/bn/translation.json";
 i18n
   .use(Backend) // Load translations from your backend
   .use(initReactI18next) // Pass i18n down to react-i18next
   .init({
-    lng: 'en', // Default language
-    fallbackLng: 'en', // Fallback language
-    interpolation: {
-      escapeValue: false, // React already does escaping
+    resources: {
+      en: { translation: en },
+      bn: { translation: bn },
     },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json', // Path to translation files
     },
