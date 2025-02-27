92af205ec7277a5f71e9c51dbea926ffe5eaf9ac
unknown <arnobm97@gmail.com>
2025-02-27 15:14:38 +0600

    asd

--- client-side/src/Pages/Founder/FounderTerms.jsx
+++ client-side/src/Pages/Founder/FounderTerms.jsx
@@ -5,15 +5,7 @@ const FounderTerms = () => {
   const { t } = useTranslation(); // Use the translation hook
 
   return (
-    <div className="lg:h-[full] lg:w-[1200px] mt-20 flex lg:flex-row sm:flex-col xs:flex-col lg:mx-auto lg:gap-12 xxs:flex -col sm:px-4 xs:px-4 xxs:px-4 sm:mb-4 xs:mb-4 xxs:mb-4 sm:h-full xs:h-full xxs:h-full">
-      <Link to="/founderpost">
-        <input
-          type="submit"
-          className="post-btn lg:h-[10%] lg:w-[100px] sm:h-[60%] xs:h-[60%] xxs:h-[60%] sm:w-[30%] xs:w-[30%] xxs:w-[30%]"
-          name="founder-post"
-          value={t("post")}
-        />
-      </Link>
+    <div className="lg:h-[full] lg:w-[1200px] mt-20 flex lg:flex-col sm:flex-col xs:flex-col lg:mx-auto lg:gap-12 xxs:flex -col sm:px-4 xs:px-4 xxs:px-4 sm:mb-4 xs:mb-4 xxs:mb-4 sm:h-full xs:h-full xxs:h-full">
       <div className="mx-auto flex flex-col gap-8">
         <h1 className="font-extrabold text-xl">
           {t("founder_terms")} {/* Use translation */}
@@ -27,6 +19,16 @@ const FounderTerms = () => {
         <p>{t("term_7")}</p> {/* Use translation */}
         <p>{t("term_8")}</p> {/* Use translation */}
         <p>{t("term_9")}</p> {/* Use translation */}
+      </div>
+      <div className="flex items-end">
+        <Link to="/founderpost">
+          <input
+            type="submit"
+            className="post-btn lg:h-[120%] lg:w-[100px] sm:h-[60%] xs:h-[60%] xxs:h-[60%] sm:w-[30%] xs:w-[30%] xxs:w-[30%]"
+            name="founder-post"
+            value={t("post")}
+          />
+        </Link>
       </div>
     </div>
   );
