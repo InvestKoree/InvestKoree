9c0d2c4c7a4e16cdfb777c4d2bba7e0d075b2db5
unknown <arnobm97@gmail.com>
2025-02-27 15:36:03 +0600

    asd

--- client-side/src/Pages/Founder/FounderTerms.jsx
+++ client-side/src/Pages/Founder/FounderTerms.jsx
@@ -5,7 +5,7 @@ const FounderTerms = () => {
   const { t } = useTranslation(); // Use the translation hook
 
   return (
-    <div className="lg:h-[full] lg:w-[1200px] mt-20 flex lg:flex-col sm:flex-col xs:flex-col lg:mx-auto lg:gap-12 xxs:flex -col sm:px-4 xs:px-4 xxs:px-4 sm:mb-4 xs:mb-4 xxs:mb-4 sm:h-full xs:h-full xxs:h-full">
+    <div className="lg:h-[full] lg:w-[1200px] mt-20 flex lg:flex-col sm:flex-col xs:flex-col lg:mx-auto lg:gap-12 xxs:flex-col sm:px-4 xs:px-4 xxs:px-4 sm:mb-4 xs:mb-4 xxs:mb-4 sm:h-full xs:h-full xxs:h-full">
       <div className="mx-auto flex flex-col gap-8">
         <h1 className="font-extrabold text-xl">
           {t("founder_terms")} {/* Use translation */}
