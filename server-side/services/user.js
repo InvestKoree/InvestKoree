5b5512b45157955b070556dedc4436bc071c90fe
unknown <arnobm97@gmail.com>
2025-03-17 12:14:05 +0600

    asdsad

--- server-side/services/user.js
+++
@@ -1,8 +0,0 @@
-// import User from '../models/userModel.js';
-
-// async function getUsers() {
-//     const users = await User.find({});
-//     return users;
-// }
-
-// export default { getUsers };
