21206e3bc87bef1fcb4b25c8c793c172287d9c0d
unknown <arnobm97@gmail.com>
2025-03-19 14:16:31 +0600

    asd

--- client-side/src/i18n.js
+++ client-side/src/i18n.js
@@ -2,8 +2,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import en from "./public/locales/en/translation.json";
-import bn from "./public/locales/bn/translation.json";
+import en from "../public/locales/en/translation.json";
+import bn from "../public/locales/bn/translation.json";
 i18n
   .use(Backend)
   .use(initReactI18next) 
