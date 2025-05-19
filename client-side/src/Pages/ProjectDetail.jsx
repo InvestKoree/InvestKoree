eac79b466df475485ebdd861cca9c748b6d352f5
unknown <arnobm97@gmail.com>
2025-05-19 12:53:04 +0600

    asdasd

--- client-side/src/Pages/ProjectDetail.jsx
+++ client-side/src/Pages/ProjectDetail.jsx
@@ -395,7 +395,7 @@ const ProjectDetail = () => {
                   {t("invest")}
                 </button>
                 {isModalOpen && (
-                  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-99">
+                  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 sm:z-99 xs:z-99 xxs:z-99">
                     <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative">
                       {/* Close Button */}
                       <button
