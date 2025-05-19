b357e99ccc6684343b97ab8366466bc31f91b952
unknown <arnobm97@gmail.com>
2025-05-19 13:34:28 +0600

    asdasd

--- client-side/src/Pages/ProjectDetail.jsx
+++ client-side/src/Pages/ProjectDetail.jsx
@@ -396,7 +396,7 @@ const ProjectDetail = () => {
                 </button>
                 {isModalOpen && (
                   <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ">
-                    <div className="bg-white p-6 rounded-lg shadow-lg lg:w-96 relative lg:z-99 sm:z-99 xs:z-99 xxs:z-99">
+                    <div className="bg-white p-6 rounded-lg shadow-lg lg:w-96 relative z-60">
                       {/* Close Button */}
                       <button
                         className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
@@ -519,9 +519,9 @@ const ProjectDetail = () => {
           </table>
         </div>
         {/* Accordion Section */}
-        <div className="mt-12 w-[80%] lg:w-[50%] xs:mb-4 xxs:mb-4 sm:mb-4 mx-auto z-1">
-          <div className="collapse collapse-plus border border-base-300 rounded-box z-1">
-            <input type="checkbox" className="peer z-1" />
+        <div className="mt-12 w-[80%] lg:w-[50%] xs:mb-4 xxs:mb-4 sm:mb-4 mx-auto z-10 ">
+          <div className="collapse collapse-plus border border-base-300 rounded-box ">
+            <input type="checkbox" className="peer " />
             <div className="collapse-title text-xl font-medium">
               {t("funding_help")}
             </div>
@@ -530,8 +530,8 @@ const ProjectDetail = () => {
             </div>
           </div>
 
-          <div className="collapse collapse-plus border border-base-300 rounded-box mt-4 z-1">
-            <input type="checkbox" className="peer z-1" />
+          <div className="collapse collapse-plus border border-base-300 rounded-box mt-4 ">
+            <input type="checkbox" className="peer " />
             <div className="collapse-title text-xl font-medium">
               {t("return_investment")}
             </div>
@@ -540,8 +540,8 @@ const ProjectDetail = () => {
             </div>
           </div>
 
-          <div className="collapse collapse-plus border border-base-300 rounded-box mt-4 z-1">
-            <input type="checkbox" className="peer z-1" />
+          <div className="collapse collapse-plus border border-base-300 rounded-box mt-4 ">
+            <input type="checkbox" className="peer " />
             <div className="collapse-title text-xl font-medium">
               {t("business_safety")}
             </div>
